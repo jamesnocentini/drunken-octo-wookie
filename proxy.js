d89fd63ccfaa1678f67da8af1bda7f140a92f55b
James Nocentini <james.nocentini@gmail.com>
2014-10-21 17:08:46 +0100

    add register_token endpoint

--- proxy.js
+++ proxy.js
@@ -1,42 +1,48 @@
 var httpProxy = require('http-proxy')
   , http = require('http')
   , concat = require('./concat')
-  , stream = require('./stream');
-var request = require('request');
-
+  , stream = require('./stream')
+  , mongo = require('./mongo')
+  , request = require('request');
 
 var proxy = httpProxy.createProxyServer();
 
-// for /concat/:playlist_id, the proxy should kick in
-// 1. given the playlist id fetch the list of steps from step up
-// 2. given the array of steps, create the final movie
-// 3. when its been concat, respond to client with {ready: true, hash: 3546788}
+  mongo.connect(function(db) {
 
-// for the rest of the request, it should pass them to stepup.io
+    var server = http.createServer(function(req, res) {
 
-var server = http.createServer(function(req, res) {
+      routes(req, res, db);
 
-  console.log(req.url);
+    });
 
-  if (req.url.indexOf('concat') > -1) {
+    server.listen(process.env.PORT || 4000);
 
-    // make request to get the playlist information
+  });
+
+
+
+function routes(req, res, db) {
+
+  if (req.url.indexOf('concat') > -1) { // concat playlist. call concat.js
+
     if (req.headers['range']) {
-      stream.byteRangeRequest(req, res)
+
+      stream.byteRangeRequest(req, res);
+
     } else {
-      // get playlist infor
+
       var id = req.url.split('/')[2];
       request('http://www.stepup.io/playlists/' + id + '.json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log(body) // Print the google web page.
           concat.concat(JSON.parse(body), id, function() {
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(JSON.stringify({"ok": true}));
-          res.end();
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({"ok": true}));
+            res.end();
 //            stream.byteRangeRequest(req, res, id)
           });
         }
-      })
+      });
 
     }
 
@@ -45,15 +51,33 @@ var server = http.createServer(function(req, res) {
     var id = req.url.split('/')[2];
     stream.byteRangeRequest(req, res, id)
 
+  } else if (req.url.indexOf('register_token') > -1) {
+
+    db.collection('tokes').findOne({token: req.body.token}, function(err, res_mongo1) {
+      if(res_mongo1) {
+        db.collection('tokens').update({token: req.body.token}, req.body, function(err, updated_user) {
+          console.log('Updated token for '+req.body.user_id+' ['+req.body.token+']');
+          res.send('token existed, updated user_id');
+        })
+      } else {
+        db.collection('tokens').insert(req.body, function(err, res_mongo2) {
+          if (err) throw err;
+          console.log('Added token for '+req.body.user_id+' ['+req.body.token+']');
+          res.send('added token with user_id');
+        });
+      }
+
+    });
+
   } else {
+
     proxy.web(req, res, {
       target: 'http://www.stepup.io',
       headers: {
         host: 'www.stepup.io'
       }
     });
+
   }
 
-});
-
-server.listen(process.env.PORT || 4000);
+}
